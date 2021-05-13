c66f1770567eb691db35352d3d5e8e3abf6b2fa4
kathtr <treptowkatharina@web.de>
2021-05-13 16:24:35 +0200

    add class when screen is smaller 640px

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hi! Welcome to My Portfolio Site")
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && screen.width < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
